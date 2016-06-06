260d3e2b4be901a38a2f3c23ea2c284ed5275e6a
云却不是雀 <479813005@qq.com>
2016-06-06 08:48:01 +0800

    增加string.Toint 扩展

---
+++ Extention/PoiExtention3.5/StringExtention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    /// <summary>
+    /// string扩展
+    /// </summary>
+    public static class StringExtention
+    {
+        /// <summary>
+        /// 使用int.Parse转换一个字符串
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int ToInt(this string s)
+        {
+            return int.Parse(s);
+        }
+    }
+}
