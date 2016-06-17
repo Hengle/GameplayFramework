c69c1393dbade19ec264bc4e4971a738fdcbee5a
云却不是雀 <479813005@qq.com>
2016-06-17 09:16:00 +0800

    增加string 到 Enum扩展

---
+++ Extention/PoiExtention3.5/EnumExtention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    /// <summary>
+    /// 枚举类的扩展
+    /// </summary>
+    public static class EnumExtention
+    {
+        public static T Parse<T>(string value)
+        {
+            return default(T);
+        }
+    }
+}
--- Extention/PoiExtention3.5/StringExtention.cs
+++ Extention/PoiExtention3.5/StringExtention.cs
@@ -19,5 +19,21 @@ namespace System
         {
             return int.Parse(s);
         }
+
+        /// <summary>
+        /// 将一个字符串转换成枚举
+        /// </summary>
+        /// <typeparam name="T">提供一个枚举类型</typeparam>
+        /// <param name="value"></param>
+        /// <returns>返回对应的枚举值</returns>
+        /// <exception cref="ArgumentException">所给泛型不是枚举</exception>
+        public static T ToEnum<T>(this string value)
+            where T:struct
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException("所给泛型不是枚举");
+
+            return  (T)Enum.Parse(typeof(T), value);
+        }
     }
 }
