eeb60908aa4543a1d5852cf71b40a43af55b0a24
云却不是雀 <479813005@qq.com>
2016-08-04 10:07:27 +0800

    扩展remove方法可以在foreach中使用，通过线程池异步实现

---
+++ Extention/PoiExtention3.5/Collections/Generic/ICollection_T_Extention.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections.Generic
+{
+    public static class ICollection_T_Extention
+    {
+        /// <summary>
+        /// 通过线程池异步移除一个元素
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ic"></param>
+        /// <param name="item"></param>
+        public static void RemoveForeach<T>(this ICollection<T> ic,T item)
+        {
+            Threading.ThreadPool.QueueUserWorkItem((obj)=> 
+            {
+                lock (ic)
+                {
+                    ic.Remove(item);
+                }
+            });
+        }
+
+        /// <summary>
+        /// 通过线程池异步添加一个元素
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ic"></param>
+        /// <param name="item"></param>
+        public static void AddForeach<T>(this ICollection<T> ic, T item)
+        {
+            Threading.ThreadPool.QueueUserWorkItem((obj) =>
+            {
+                lock (ic)
+                {
+                    ic.Add(item);
+                }
+            });
+        }
+    }
+}
