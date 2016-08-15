550f05cf0a7220dc043e1fac54583f846178607d
云却不是雀 <479813005@qq.com>
2016-08-15 10:34:11 +0800

    增加简易加密

--- Extention/PoiExtention3.5/StringExtention.cs
+++ Extention/PoiExtention3.5/StringExtention.cs
@@ -46,5 +46,58 @@ namespace System
         {
             return value == "true"|| value == "TRUE"||value == "True";
         }
+
+        #region 加密相关
+
+        const int DefaultSeed = 63;
+
+        /// <summary>
+        /// 对字符串进行轻量的加密，使用Decrypt方法解密
+        /// </summary>
+        /// <param name="original"></param>
+        /// <param name="Seed">种子</param>
+        /// <returns></returns>
+        public static string Encipher(this string original,int Seed = 0)
+        {
+            var array = original.ToCharArray();
+            var tempseed = GetSeed(Seed);
+            string result = "";
+            foreach (var item in array)
+            {
+                int tempvalue = (int)item;
+                result += (tempvalue + tempseed).ToString() + ".";
+            }
+
+            return result;
+        }
+
+        private static int GetSeed(int seed)
+        {
+            return seed == 0 ? DefaultSeed : seed;
+        }
+
+        /// <summary>
+        /// 解密密文
+        /// </summary>
+        /// <param name="ciphertext"></param>
+        /// <param name="Seed">种子</param>
+        /// <returns></returns>
+        public static string Decrypt(this string ciphertext,int Seed = 0)
+        {
+            string[] result = ciphertext.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var tempseed = GetSeed(Seed);
+
+            string original = "";
+            foreach (var item in result)
+            {
+                int tempvalue = item.ToInt();
+                char temp = (char)(tempvalue-tempseed);
+                original += temp;
+            }
+            return original;
+        }
+
+        #endregion
     }
 }
