e328e32969331effb8e8a867cf4f0dbd1d78e326
云却不是雀 <479813005@qq.com>
2016-08-15 10:34:57 +0800

    增加测试用工程

---
+++ Extention/TestFunc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "stedssdfasfsdff2343sdf*&^&^adf";
+            Console.WriteLine(test);
+            string temp = test.Encipher();
+            Console.WriteLine(temp);
+            string original = temp.Decrypt();
+            Console.WriteLine(original);
+            Console.ReadLine();
+        }
+    }
+}
