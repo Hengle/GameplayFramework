e9b6cd38faab7356018222d46fd07b31c3ed6b45
云却不是雀 <479813005@qq.com>
2016-12-05 16:53:43 +0800

    no message

---
+++ Value/Script/Pawn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poi
+{
+    public class Pawn
+    {
+        protected DataInfo dataInfo;
+        public void Init(DataInfo Info)
+        {
+            dataInfo = Info;
+        }
+
+        public PawnInfo DataInfo
+        {
+            get
+            {
+                return dataInfo as PawnInfo;
+            }
+        }
+    }
+}
