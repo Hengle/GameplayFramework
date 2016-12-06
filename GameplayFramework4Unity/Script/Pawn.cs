b3f9d52f40c59bad0ba8e20185fc6fd798130832
云却不是雀 <479813005@qq.com>
2016-12-06 09:00:35 +0800

    完善Pawn

--- GameplayFramework4Unity/Script/Pawn.cs
+++ GameplayFramework4Unity/Script/Pawn.cs
@@ -5,14 +5,27 @@ using System.Text;
 
 namespace Poi
 {
+    /// <summary>
+    /// 角色
+    /// </summary>
     public class Pawn
     {
+        /// <summary>
+        /// 角色信息（数据模型）
+        /// </summary>
         protected DataInfo dataInfo;
+        /// <summary>
+        /// 通过数据模型初始化
+        /// </summary>
+        /// <param name="Info"></param>
         public void Init(DataInfo Info)
         {
             dataInfo = Info;
         }
 
+        /// <summary>
+        /// 角色信息（数据模型）
+        /// </summary>
         public PawnInfo DataInfo => dataInfo as PawnInfo;
         
     }
