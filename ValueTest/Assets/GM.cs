7735ae48cf9a774f58548f3981f9265444530780
云却不是雀 <479813005@qq.com>
2016-12-08 16:32:03 +0800

    no message

---
+++ ValueTest/Assets/GM.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.SceneManagement.SceneManager;
+
+public class GM : MonoBehaviour {
+
+    public static GM GMInstance;
+
+    // 加载脚本实例时调用 Awake
+    private void Awake()
+    {
+        GMInstance = this;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        LoadSceneAsync(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
