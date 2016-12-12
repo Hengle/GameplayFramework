ae41ab3e4ed07e4a04086aa8dd29fd136bcee258
云却不是雀 <479813005@qq.com>
2016-12-12 10:57:42 +0800

    no message

---
+++ ValueTest/Assets/MonsterManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject[] Monsters;
+
+    public Transform pos;
+	// Use this for initialization
+	void Start ()
+    {
+        CreateMonster();
+    }
+
+    private void CreateMonster()
+    {
+        GameObject mon = Instantiate(Monsters[0], pos.position, pos.rotation);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
