756ddbfed7455fa3fe4f23a9b2a0d5a684fbdada
云却不是雀 <479813005@qq.com>
2016-12-12 14:12:50 +0800

    no message

--- ValueTest/Assets/Monster.cs
+++ ValueTest/Assets/Monster.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using UnityEngine.AI;
 
 namespace Poi
 {
@@ -10,6 +13,42 @@ namespace Poi
         /// <summary>
         /// 怪物信息（数据模型）
         /// </summary>
-        public new MonsterInfo DataInfo => dataInfo as MonsterInfo; 
+        public new MonsterInfo DataInfo => dataInfo as MonsterInfo;
+
+        [SerializeField]
+        public Transform[] destinations = new Transform[3];
+
+        public void Move()
+        {
+            var r = new System.Random();
+            int a = r.Next(destinations.Length);
+            agent?.SetDestination(destinations[a].position);
+        }
+
+        private NavMeshAgent agent;
+        // 仅在首次调用 Update 方法之前调用 Start
+        private void Start()
+        {
+            var pos = GameObject.Find("Pos (3)");
+            destinations[0] = pos.transform;
+            var pos1 = GameObject.Find("Pos (1)");
+            destinations[1] = pos1.transform;
+            var pos2 = GameObject.Find("Pos (2)");
+            destinations[2] = pos2.transform;
+
+
+            agent = GetComponent<NavMeshAgent>();
+
+            StartCoroutine(RandomMove());
+        }
+
+        private IEnumerator RandomMove()
+        {
+            while (true)
+            {
+                Move();
+                yield return new WaitForSeconds(10);
+            }
+        }
     }
 }
