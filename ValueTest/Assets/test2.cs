a639bed201e034bf098df984112f975ca186f9fe
云却不是雀 <479813005@qq.com>
2016-12-13 15:33:12 +0800

    no message

---
+++ ValueTest/Assets/test2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Tiao());
+
+	}
+
+    private IEnumerator Tiao()
+    {
+        yield return new WaitForSeconds(3);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("B");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
