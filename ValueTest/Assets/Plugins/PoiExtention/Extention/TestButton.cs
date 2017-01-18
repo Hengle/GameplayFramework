3eaaa4f6e4eda00a0540f838d8c9accd914f9ce4
云却不是雀 <479813005@qq.com>
2017-01-18 09:54:52 +0800

    增加一个测试脚本

---
+++ ValueTest/Assets/Plugins/PoiExtention/Extention/TestButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+
+    public Rect Rect = new Rect(10,10,40,25);
+
+    public UnityEngine.UI.Button.ButtonClickedEvent Event;
+
+    private void OnGUI()
+    {
+        if (GUI.Button(Rect,"Test"))
+        {
+            if (Event != null)
+            {
+                Event.Invoke();
+            }
+        }
+    }
+
+#endif
+
+}
