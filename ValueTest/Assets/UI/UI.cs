ac350338fd69c7674038312fbeac959f9568fd7c
云却不是雀 <479813005@qq.com>
2017-01-20 11:25:49 +0800

    增加UI

---
+++ ValueTest/Assets/UI/UI.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class UI
+{
+    public static readonly UI Instance = new UI();
+    private UI() { }
+}
