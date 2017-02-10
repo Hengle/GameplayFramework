7c56aff7e55f00115975aee373b241a70ce0265f
云却不是雀 <479813005@qq.com>
2017-02-10 15:01:40 +0800

    准星

---
+++ ValueTest/Assets/CursorFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollow : MonoBehaviour {
+
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    readonly Vector3 v = new Vector3(0, 0, 1);
+	// Update is called once per frame
+	void Update () {
+        var pos = cam.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = pos + v;
+	}
+}
