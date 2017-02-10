dd6d4d3b36b4bd233ef91f464611a1bcb621d690
云却不是雀 <479813005@qq.com>
2017-02-10 10:54:30 +0800

    UI跟随鼠标

--- ValueTest/Assets/Scenes/CursorPanel.cs
+++ ValueTest/Assets/Scenes/CursorPanel.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class CursorPanel : MonoBehaviour
 {
+    public Canvas can;
+    RectTransform rect;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rect = transform as RectTransform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(can.transform as RectTransform, Input.mousePosition, null, out pos))
+        {
+            rect.localPosition = pos;
+        }
+    }
 }
