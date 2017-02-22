934746b83d46538578c64e4bcfbeecf3c7ae608b
云却不是雀 <479813005@qq.com>
2017-02-22 10:14:14 +0800

    接口调整

---
+++ ValueTest/Assets/PoiGameplayFramework/UIInterface/IUITarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Poi
+{
+    public interface IUITarget
+    {
+        int ID { get; }
+        Vector3 ScreenPosition { get; }
+    }
+
+}
--- ValueTest/Assets/Scripts/MyCursor.cs
+++ ValueTest/Assets/Scripts/MyCursor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Poi;
 using UnityEngine;
 
 public class MyCursor : MonoBehaviour
