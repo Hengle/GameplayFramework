b31bc03c698080c7a1ef319d81a260ae238ea6bd
云却不是雀 <479813005@qq.com>
2017-02-24 16:52:09 +0800

    颜色

---
+++ ValueTest/Assets/TargetPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TargetPoint : MonoBehaviour {
+    public Mesh PlayerStartMesh;
+    // Use this for initialization
+    public Color PlayerStartColor  = Color.HSVToRGB(125,137,277);
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawSphere(transform.position, 1);
+        //Gizmos.DrawWireMesh(Mesh.)
+        Gizmos.color = PlayerStartColor;
+        Gizmos.DrawWireMesh(PlayerStartMesh,transform.position + Vector3.up,transform.rotation);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position + Vector3.up,
+            transform.position + Vector3.up + transform.forward * 1.5f);
+    }
+
+
+}
