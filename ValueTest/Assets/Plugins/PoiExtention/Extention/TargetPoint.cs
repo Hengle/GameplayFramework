8bca5c389f1dd24072c804622bd05944cd45bd73
云却不是雀 <479813005@qq.com>
2017-02-27 13:13:31 +0800

    标记点脚本调整

--- ValueTest/Assets/Plugins/PoiExtention/Extention/TargetPoint.cs
+++ ValueTest/Assets/Plugins/PoiExtention/Extention/TargetPoint.cs
@@ -1,28 +1,120 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 标记点脚本
+/// <para>用来在Scene标记一个空的GameObject</para>
+/// </summary>
 [ExecuteInEditMode]
-public class TargetPoint : MonoBehaviour {
+public class TargetPoint : MonoBehaviour
+{
+    [Tooltip("游戏中是否可见（暂时不起作用）")]
     public bool isShowInGame = false;
+    [Tooltip("是否中心调整到模型底部")]
+    public bool centerDown = true;
+    [Tooltip("网格类型")]
+    public PrimitiveType type = PrimitiveType.Capsule;
+    public Vector3 scale = Vector3.one;
+    Mesh pointMesh;
+    public Color targetColor  = new Color32(105,227,116,41);
 
+    PrimitiveType old = PrimitiveType.Capsule;
 
+    private void Awake()
+    {
+        CheckMesh();
+    }
 
-    public Mesh PlayerStartMesh;
-    // Use this for initialization
-    public Color PlayerStartColor  = new Color32(105,227,116,41);
+    private void Update()
+    {
+        CheckMesh();
+    }
+
+    /// <summary>
+    /// 检查mesh变动
+    /// </summary>
+    private void CheckMesh()
+    {
+        if (!pointMesh || type != old)
+        {
+            GameObject temp = GameObject.CreatePrimitive(type);
+            Mesh capsule = temp.GetComponent<MeshFilter>().sharedMesh;
+            pointMesh = capsule;
+#if UNITY_EDITOR
+            DestroyImmediate(temp);
+#else  
+            Destroy(temp);
+#endif
+
+            old = type;
+        }
+    }
 
     private void OnDrawGizmos()
     {
-        DrawPlayerStart();
+        switch (type)
+        {
+            case PrimitiveType.Sphere:
+                DrawSphere();
+                break;
+            case PrimitiveType.Capsule:
+                DrawPlayerStart();
+                break;
+            case PrimitiveType.Cylinder:
+                break;
+            case PrimitiveType.Cube:
+                DrawCube();
+                break;
+            case PrimitiveType.Plane:
+                break;
+            case PrimitiveType.Quad:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void DrawSphere()
+    {
+        Gizmos.color = targetColor;
+        Vector3 meshCenter = transform.position;
+        if (centerDown)
+        {
+            meshCenter = transform.position + Vector3.up * 0.5f * scale.y;
+        }
+
+
+        Gizmos.DrawMesh(pointMesh, meshCenter, transform.rotation, scale);
+    }
+
+    private void DrawCube()
+    {
+        Gizmos.color = targetColor;
+        Vector3 meshCenter = transform.position;
+        if (centerDown)
+        {
+            meshCenter = transform.position + Vector3.up * 0.5f * scale.y;
+        }
+
+
+        Gizmos.DrawMesh(pointMesh, meshCenter,transform.rotation, scale);
     }
 
     private void DrawPlayerStart()
     {
-        Gizmos.color = PlayerStartColor;
-        Gizmos.DrawWireMesh(PlayerStartMesh, transform.position + Vector3.up, transform.rotation);
+        Gizmos.color = targetColor;
+        Vector3 meshCenter = transform.position;
+
+        if (centerDown)
+        {
+            meshCenter = transform.position + Vector3.up * scale.y;
+        }
+
+        Gizmos.DrawWireMesh(pointMesh, meshCenter, transform.rotation,scale);
+
         Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position + Vector3.up,
-            transform.position + Vector3.up + transform.forward * 1.5f);
+        Gizmos.DrawLine(meshCenter,meshCenter + transform.forward * 1.5f);
     }
 }
