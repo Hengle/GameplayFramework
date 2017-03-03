5823df7b54211c3735102434851d8fbc62181956
云却不是雀 <479813005@qq.com>
2017-03-03 13:55:57 +0800

    增加控制台初步

---
+++ ValueTest/Assets/GM CMD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public partial class GM
+{
+    public void CMD(string cmdtest)
+    {
+
+    }
+}
