04b1b0f5918a4e6ce16bbb2569f1b944dd8bb7c8
云却不是雀 <479813005@qq.com>
2017-03-31 15:09:27 +0800

    切换模型

---
+++ ValueTest/Assets/ModelList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelList : MonoBehaviour
+{
+
+    public List<ModelList> List;
+}
+
+
+public class ModelTemplate : ScriptableObject
+{
+    public int test;
+}
